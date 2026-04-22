7350b90e2acdab239a1f917c15e5ddd202f5bf37
Nour Khaled Abdelrhman Elsaid <N.Elsaid27003@student.aast.edu>
2026-04-22 21:07:16 +0000

    third commit

---
+++ comments.js
@@ -0,0 +1,13 @@
+// Create web server
+// The code below should be suggested by Copilot:
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(3000, '127.0.0.1', () => {
+    console.log('Server running at http://127.0.0.1:3000/');
+});
